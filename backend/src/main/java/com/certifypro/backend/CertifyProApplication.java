e919ba51cb3458757753948cc2a639acbd8add72
Mohiddin <2400030165@kluniversity.in>
2026-05-01 11:30:57 +0530

    Speed up OTP email flow

--- backend/src/main/java/com/certifypro/backend/CertifyProApplication.java
+++ backend/src/main/java/com/certifypro/backend/CertifyProApplication.java
@@ -3,9 +3,11 @@ package com.certifypro.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableAsync
 public class CertifyProApplication {
     public static void main(String[] args) {
         SpringApplication.run(CertifyProApplication.class, args);
